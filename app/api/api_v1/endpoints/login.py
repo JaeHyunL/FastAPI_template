2a3fa9a144dedfa4ff94154529d8afd5159a5e6f
JaeHyunL <swaa23@gmail.com>
2023-02-06 17:52:47 +0900

    feat(utils): add generate_password_rest_token

--- app/api/api_v1/endpoints/login.py
+++ app/api/api_v1/endpoints/login.py
@@ -1,3 +1,15 @@
-from fastapi import APIRouter
+from datetime import timedelta
+from typing import Any
+
+from fastapi import APIRouter, Body, Depends, HTTPException
+from fastapi.security import OAuth2PasswordRequestForm
+from sqlalchemy.orm import Session
+
+from app import crud, models, schemas
+from app.api import deps
+from app.core import security
+from app.core.config import settings
+from app.core.security import get_password_hash
+from app.utils
 
 router = APIRouter()
---
+++ app/utils.py
@@ -0,0 +1,21 @@
+from datetime import datetime, timedelta
+from jose import jwt
+
+from app.core.config import settings
+
+
+def generate_password_rest_token(email: str) -> str:
+    delta = timedelta(hours=settings.EMAIL_RESET_TOKEN_EXPIRE_HOURS)
+    now = datetime.now()
+    expires = now + delta
+    exp = expires.timestamp()
+    encoded_jwt = jwt.encode(
+        {
+            "exp": exp,
+            "nbf": now,
+            "sub": email,
+        }, settings.SECRET_KEY,
+        algorithm="HS256",
+    )
+    return encoded_jwt
+
