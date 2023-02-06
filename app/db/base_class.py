6a3d75354cd6211ecc10fb548d906822d0696a07
JaeHyunL <swaa23@gmail.com>
2023-02-06 17:21:50 +0900

    fix(db init): 데이터베이스 생성 관련
    <body>
     - 기존 종속성 alembic 걷어내기(정확한 사용법을 몰라서 제거)
     - declative_base 관련 이슈사항 해결.
    </body>

--- app/db/base_class.py
+++ app/db/base_class.py
@@ -1,10 +1,23 @@
+from typing import Any
+from datetime import datetime
+
+from sqlalchemy import Column, DateTime
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy.ext.declarative import declarative_base
+
+
+Base = declarative_base()
 
 
 @as_declarative()
 class Base:
-    id: any
+    id: Any
+    created_at = Column(DateTime, default=datetime.now)
+    updated_at = Column(DateTime, default=datetime.now, onupdate=datetime.now)
+
     __name__: str
+    # Generate __tablename__ automatically
+
     @declared_attr
     def __tablename__(cls) -> str:
         return cls.__name__.lower()
--- app/models/item.py
+++ app/models/item.py
@@ -10,6 +10,7 @@ if TYPE_CHECKING:
 
 
 class Item(Base):
+
     id = Column(Integer, primary_key=True, index=True)
     title = Column(String, index=True)
     description = Column(String, index=True)
